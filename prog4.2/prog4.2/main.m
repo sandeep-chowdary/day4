d8553b052cfe617160482f7a864b4864ca25314f
sandeep-chowdary <sandeep.pavulurii@gmail.com>
2014-05-08 15:17:29 -0400

    day4.2

---
+++ prog4.2/prog4.2/main.m
@@ -0,0 +1,23 @@
+//
+//  main.m
+//  prog4.2
+//
+//  Created by User1 on 2014-05-08.
+//  Copyright (c) 2014 dhaumik. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        int count = 1;
+        
+        while (count <=5)
+            NSLog (@"%i",count);
+            ++count;
+     
+    }
+    return 0;
+}
